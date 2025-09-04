6d25158f05b1cbc466769d38ba60916bd8b02003
ShoaibHassan11 <soaebhasan12@users.noreply.github.com>
2025-09-04 22:32:02 +0530

    Routers

--- storefront2/store/urls.py
+++ storefront2/store/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
+from rest_framework.routers import SimpleRouter
 from . import views
+# from pprint import pprint
+
+router = SimpleRouter()
+router.register('products', views.ProductViewSet)
+router.register('collection', views.CollectionViewSet)
+# pprint(router.urls)
 
 # URLConf
-urlpatterns = [
-    path('products/', views.ProductList.as_view()),
-    path('products/<int:pk>/', views.ProductDetail.as_view()),
-    path('collections/', views.CollectionList.as_view()),
-    path('collections/<int:pk>/', views.CollectionDetail.as_view()),
-]
+urlpatterns = router.urls
