89c5cd618e005622e28c1ee56cb7bb92ef902f33
ShoaibHassan11 <soaebhasan12@users.noreply.github.com>
2025-09-08 21:38:02 +0530

    add django_filter in INSTALLED_APPS and update storefront database to storefront2 in DATABASES

--- storefront2/storefront/settings.py
+++ storefront2/storefront/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_filters',
     'rest_framework',
     'playground',
     'debug_toolbar',
@@ -90,7 +91,7 @@ WSGI_APPLICATION = 'storefront.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'storefront',
+        'NAME': 'storefront2',
         'HOST': 'localhost',
         'USER': 'root',
         'PASSWORD': 'sS#190845#Ss'
