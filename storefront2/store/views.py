7f94bd3b7082b476af52ae055043e60156c0b201
ShoaibHassan11 <soaebhasan12@users.noreply.github.com>
2025-09-08 21:33:48 +0530

    Generic Filtering

--- storefront2/store/views.py
+++ storefront2/store/views.py
@@ -1,6 +1,7 @@
 from django.db.models.aggregates import Count
 from django.shortcuts import get_object_or_404
 from django.http import HttpResponse
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.mixins import ListModelMixin, CreateModelMixin
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
@@ -10,20 +11,23 @@ from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIV
 from rest_framework import status
 from .models import Product, Collection, OrderItem, Review
 from .serializers import ProductSerializer, CollectionSerializer, ReviewSerializer
+from .filters import ProductFilter
 
 
 class ProductViewSet(ModelViewSet):
-    # queryset = Product.objects.all()
+    queryset = Product.objects.all()
     serializer_class = ProductSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_class = ProductFilter
 
-    def get_queryset(self):
-        queryset = Product.objects.all()
-        collection_id = self.request.query_params.get('collection_id')
-        if collection_id is not None:
-            queryset = queryset.filter(collection_id=collection_id)
+    # def get_queryset(self):
+    #     queryset = Product.objects.all()
+    #     collection_id = self.request.query_params.get('collection_id')
+    #     if collection_id is not None:
+    #         queryset = queryset.filter(collection_id=collection_id)
 
-        return queryset
-        # return Product.objects.filter(collection_id=self.request.query_params['collection_id'])
+    #     return queryset
+    #     # return Product.objects.filter(collection_id=self.request.query_params['collection_id'])
 
     def get_serializer_context(self):
         return {'request': self.request}
