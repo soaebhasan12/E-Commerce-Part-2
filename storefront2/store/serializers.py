33d5451ca5abedf25d5ad746784860d25ea5d549
ShoaibHassan11 <soaebhasan12@users.noreply.github.com>
2025-09-10 20:43:34 +0530

    create CartSerializer

--- storefront2/store/serializers.py
+++ storefront2/store/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from store.models import Product, Collection, Review
+from store.models import Product, Collection, Review, Cart
 from decimal import Decimal
 
 
@@ -32,6 +32,13 @@ class ReviewSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         product_id = self.context['product_id']
         return Review.objects.create(product_id=product_id, **validated_data)
+    
+
+
+class CartSerializer(serializers.Serializer):
+    class Meta:
+        model = Cart
+        fields = ['id', 'created_at']
 
         
 
