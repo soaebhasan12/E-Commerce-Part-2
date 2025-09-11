b83ba4fab61eeaae6c21df5f249b2c2335a86e86
ShoaibHassan11 <soaebhasan12@users.noreply.github.com>
2025-09-11 08:31:54 +0530

    Deleting Cart Item - viewslpy: add elif condition to delete a cart item (PATCH) and add http_method_name to prevent conflict with PUT request. import UpdateCartItemSerializer from serializers.py

--- storefront2/store/views.py
+++ storefront2/store/views.py
@@ -12,7 +12,7 @@ from rest_framework import status
 from .filters import ProductFilter
 from .pagination import DefaultPagination
 from .models import Product, Collection, OrderItem, Review, Cart, CartItem
-from .serializers import ProductSerializer, CollectionSerializer, ReviewSerializer,          CartSerializer, CartItemSerializer, AddCartItemSerializer
+from .serializers import ProductSerializer, CollectionSerializer, ReviewSerializer,          CartSerializer, CartItemSerializer, AddCartItemSerializer, UpdateCartItemSerializer
 
 
 
@@ -82,10 +82,13 @@ class CartViewSet(CreateModelMixin,
 
 class CartItemViewSet(ModelViewSet):
     # serializer_class = CartItemSerializer
+    http_method_names = ['get', 'post', 'patch', 'delete']
 
     def get_serializer_class(self):
         if self.request.method == 'POST':
             return AddCartItemSerializer
+        elif self.request.method == 'PATCH':
+            return UpdateCartItemSerializer
         return CartItemSerializer
     
     def get_serializer_context(self):
